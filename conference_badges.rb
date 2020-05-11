47fabc673ce2a28182b4e0f57483e440c257fe17
Tyler Firth <tyfirth@gmail.com>
2020-05-11 20:48:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ guests = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 rooms = [1,2,3,4,5,6,7]
 
-def badge_maker(name)
+def badge_maker(guests)
   guests.each{ |name| puts "Hello, my name is #{name}." }
 end
 
