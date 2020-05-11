1827d782dfd5bd3f55588e021afd2d0681987874
Tyler Firth <tyfirth@gmail.com>
2020-05-11 20:48:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ guests = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = [1,2,3,4,5,6,7]
 
 def badge_maker(guests)
-  guests.each{ |name| puts "Hello, my name is #{name}." }
+  guests.each { |name| puts "Hello, my name is #{name}." }
 end
 
 def batch_badge_creator(guests)
